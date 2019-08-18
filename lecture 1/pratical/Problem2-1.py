36c64f594a1e7228b8a1b0d66c7e5f3a6199cc8a
Ars1991 <minasyan.arsen@mail.ru>
2019-08-18 13:39:31 +0400

    final

---
+++ lecture 1/pratical/Problem2-1.py
@@ -0,0 +1,5 @@
+import math
+AB=3
+AC=4
+BC=int((AB**2+AC**2)**0.5)
+print(BC)
---
+++ lecture 1/pratical/problem-1.py
@@ -0,0 +1,14 @@
+course =  "Python"
+student= "Arsen Minasyan"
+nickname= "Ars1991"
+grade= "100"
+python_expert= True
+print("course=" ,course, "\n"
+      
+  "student =",student, "\n"
+      
+     "nickname =",nickname, "\n"
+       
+     "grade =",grade, "\n"
+      
+     "python_expert =",python_expert) 
